61a6e9c53d003704bf90bed3d7620524f4f300b9
Luis Montes <monteslu@gmail.com>
2020-10-19 15:50:43 -0700

    step count

--- src/App.js
+++ src/App.js
@@ -1,61 +1,76 @@
 import React, { useEffect, useState} from 'react';
 import './App.css';
+import { pedometer } from 'pedometer';
 
 let x = 0;
 let y = 0;
 let z = 0;
 
-let x2 = 0;
-let y2 = 0;
-let z2 = 0;
-
 let a = 0;
 let b = 0;
 let g = 0;
 
-let count = 0;
+// let count = 0;
+
+let accs = [];
+let gyros = [];
+
+const DEG_TO_RADS =  Math.PI / 180;
 
 
 
-    window.addEventListener("deviceorientation", (e) => {
-      // console.log('orientat', e.alpha, e.beta, e.gamma);
-      // setOrientation(e);
-      a = e.alpha;
-      b = e.beta;
-      g = e.gamma;
-      count++;
+  window.addEventListener("deviceorientation", (e) => {
+    // console.log('orientat', e.alpha, e.beta, e.gamma);
+    // setOrientation(e);
+    a = e.alpha;
+    b = e.beta;
+    g = e.gamma;
+    // count++;
 
-    }, true);
+  }, true);
 
-    window.addEventListener("devicemotion", (e) => {
-      // console.log('orientat', e.alpha, e.beta, e.gamma);
-      // setOrientation(e);
-      x = e.acceleration.x;
-      y = e.acceleration.y;
-      z = e.acceleration.z;
+  window.addEventListener("devicemotion", (e) => {
+    // console.log('orientat', e.alpha, e.beta, e.gamma);
+    // setOrientation(e);
+    x = e.acceleration.x;
+    y = e.acceleration.y;
+    z = e.acceleration.z;
 
-      x2 = e.accelerationIncludingGravity.x;
-      y2 = e.accelerationIncludingGravity.y;
-      z2 = e.accelerationIncludingGravity.z;
-      
-      // count++;
+    // x2 = e.accelerationIncludingGravity.x;
+    // y2 = e.accelerationIncludingGravity.y;
+    // z2 = e.accelerationIncludingGravity.z;
+    
+    // count++;
 
-    }, true);
+  }, true);
 
-    window.setInterval(() => {
-      // setOrientation([a, b, g]);
-      console.log(a, b, g, count);
-    }, 1000);
+  window.setInterval(() => {
+   
+    accs.push([x, y, z]);
+    
+    // pitch roll yaw (beta, gamma, alpha)
+    gyros.push([b * DEG_TO_RADS, g * DEG_TO_RADS, a * DEG_TO_RADS]);
 
+  }, 50);
+
+let steps = 0;
+
+function calcSteps() {
+  const res = pedometer(accs, gyros, 20, {});
+  steps = res.length;
+  console.log([x, y, z, a, b, g], steps, res);
+  accs = [];
+  gyros = [];
+}
 
 function App() {
-  const [orientation, setOrientation] = useState([x, y, z, x2, y2, z2, a, b, g]);
+  const [orientation, setOrientation] = useState([x, y, z, a, b, g]);
   // const [motion, setMotion] = useState({x: 0, y: 0, z: 0});
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     setTimeout(() => {
-      setOrientation([x, y, z, x2, y2, z2, a, b, g]);
+      setOrientation([x, y, z, a, b, g]);
     }, 200);
   });
 
@@ -66,14 +81,16 @@ function App() {
         <div>{Number(orientation[0]).toFixed(2)}</div>
         <div>{Number(orientation[1]).toFixed(2)}</div>
         <div>{Number(orientation[2]).toFixed(2)}</div>
-        <br/><br/><div>accel + grav</div>
-        <div>{Number(orientation[3]).toFixed(2)}</div>
-        <div>{Number(orientation[4]).toFixed(2)}</div>
-        <div>{Number(orientation[5]).toFixed(2)}</div>
         <br/><br/><div>orientation</div>
-        <div>{Math.round(orientation[6])}</div>
-        <div>{Math.round(orientation[7])}</div>
-        <div>{Math.round(orientation[8])}</div>
+        <div>{Math.round(orientation[3])}</div>
+        <div>{Math.round(orientation[4])}</div>
+        <div>{Math.round(orientation[5])}</div>
+        <br/><br/>
+        <button onClick={calcSteps}>
+          calc
+        </button>
+        <br/><br/>
+        <div>{steps}</div>
       </header>
     </div>
   );
